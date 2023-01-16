2c0b6ff503d24bbac63199809e2b7c1492bf5f3f
gavingoh99 <gavingoh99@gmail.com>
2023-01-16 17:32:13 +0800

    feat: minor ui updates

--- src/components/issuepage/MaturitySlider.tsx
+++ src/components/issuepage/MaturitySlider.tsx
@@ -3,20 +3,22 @@ import { FC, useState } from 'react';
 const MaturitySlider: FC = () => {
   const [val, setVal] = useState('5');
   return (
-    <div className='flex justify-center gap-10'>
-      <input
-        //eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        //@ts-ignore
-        style={{ accentColor: '#ef4444' }}
-        onChange={(event) => setVal(event.target.value)}
-        className='w-1/2'
-        type='range'
-        min='.5'
-        max='10'
-        value={val}
-        step='.5'
-      />
-      <div>{val} years</div>
+    <div className='flex justify-center'>
+      <div className='flex w-2/3 flex-col gap-5'>
+        <input
+          //eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          //@ts-ignore
+          style={{ accentColor: '#ef4444' }}
+          onChange={(event) => setVal(event.target.value)}
+          className='grow'
+          type='range'
+          min='.5'
+          max='10'
+          value={val}
+          step='.5'
+        />
+        <div className='self-center'>{val} years</div>
+      </div>
     </div>
   );
 };

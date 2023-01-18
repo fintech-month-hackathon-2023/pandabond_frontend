5de4e30e8f13371ccda9de63a80bb7d962fb938f
ysjprojects <85871692+ysjprojects@users.noreply.github.com>
2023-01-18 13:27:35 +0800

    feat: turned off warning for unused-imports/no-unused-vars

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,7 +28,7 @@ module.exports = {
     '@typescript-eslint/no-unused-vars': 'off',
     'unused-imports/no-unused-imports': 'warn',
     'unused-imports/no-unused-vars': [
-      'warn',
+      'off',
       {
         vars: 'all',
         varsIgnorePattern: '^_',
